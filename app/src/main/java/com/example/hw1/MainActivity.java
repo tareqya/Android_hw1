2c6912a41f6ad515cd94e3519cbd83ac1e687b6e
maor7 <maor7043@gmail.com>
2021-11-03 18:24:29 +0200

    spaces

--- app/src/main/java/com/example/hw1/MainActivity.java
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //FindViews();
